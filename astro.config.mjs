ff02cd4c1c90a304b4cd0fdef455d926b415d857
vallode <18506096+vallode@users.noreply.github.com>
2026-02-16 00:46:12 -0800

    Add nixpacks config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mariuszz.com/",
+  output: "static",
   integrations: [mdx(), sitemap()],
   vite: {
     define: {
