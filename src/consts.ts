b3697d82594c1a9d0bfe7421f9fd6e76df9a537b
vallode <18506096+vallode@users.noreply.github.com>
2026-02-16 02:27:39 -0800

    Add robots directive

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Mariusz Zarzycki";
-export const SITE_DESCRIPTION = "TODO";
+export const SITE_DESCRIPTION = "Freelance web developer, tinkerer, and hobbyist. Creating a more efficient and accessible web.";
