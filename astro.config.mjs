50b999287e4fc97a3fd24c78648dd5ca0d069352
vallode <18506096+vallode@users.noreply.github.com>
2026-02-16 21:34:29 -0800

    Fix import aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
 
-// https://astro.build/config
+/** https://astro.build/config */
 export default defineConfig({
   site: "https://mariuszz.com/",
   output: "static",
@@ -12,6 +13,17 @@ export default defineConfig({
   vite: {
     define: {
       __BUILD_DATE__: JSON.stringify(Temporal.Now.instant().toString()),
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@/components": fileURLToPath(
+          new URL("./src/components", import.meta.url),
+        ),
+        "@/layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@/assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@/styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      },
     },
   },
 });
