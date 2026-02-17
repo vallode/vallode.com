4be1dcc47d82f25d8cee966c17ef63492abfd0d1
vallode <18506096+vallode@users.noreply.github.com>
2026-02-16 16:33:53 -0800

    Fix schema

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Mariusz Zarzycki";
-export const SITE_DESCRIPTION = "Freelance web developer, tinkerer, and hobbyist. Creating a more efficient and accessible web.";
+export const SITE_DESCRIPTION =
+  "Freelance web developer, tinkerer, and hobbyist. Creating a more efficient and accessible web.";
