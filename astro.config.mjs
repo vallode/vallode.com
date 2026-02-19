245f624e82044df85860a6507117e3908cf3a437
vallode <18506096+vallode@users.noreply.github.com>
2026-02-18 21:32:37 -0800

    Add inlining stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,19 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import { fileURLToPath } from "node:url";
 
 /** https://astro.build/config */
 export default defineConfig({
   site: "https://mariuszz.com/",
   output: "static",
+  compressHTML: true,
   integrations: [mdx(), sitemap()],
   vite: {
     define: {
       __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
     },
   },
+  build: {
+    inlineStylesheets: "always",
+  }
 });
