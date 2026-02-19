8d0842681aed0bb6a7a310470181fd3fd3eecdd2
vallode <18506096+vallode@users.noreply.github.com>
2026-02-18 21:07:46 -0800

    Fix alises

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,18 +12,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   vite: {
     define: {
-      __BUILD_DATE__: JSON.stringify(Temporal.Now.instant().toString()),
-    },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-        "@/components": fileURLToPath(
-          new URL("./src/components", import.meta.url),
-        ),
-        "@/layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
-        "@/assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-        "@/styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
-      },
+      __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
     },
   },
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts.ts";
+import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts.ts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
